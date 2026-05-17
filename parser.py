7864aa578fa3edae98c2ae14dd7f6e10b2eb55ec
dsadep <polakovk421@gmail.com>
2026-05-17 18:29:35 +1000

    logical operands, comparison, unary signs and powers

--- parser.py
+++ parser.py
@@ -4,52 +4,110 @@ class Parser:
         self.pos = 0 
 
     def get_current(self):
-        token = self.tokens[self.pos]
-        if self.tokens[self.pos] == 'EOF': 
+        token = self.tokens[self.pos].get('value')
+        if token == 'EOF': 
             return 'EOF'
-        return token.get('value')
+        return token
     
     def eat(self):
-        if self.tokens[self.pos + 1] != 'EOF':
+        if self.pos < len(self.tokens) - 1:
             self.pos += 1
-        else:
-            return 'EOF'
-    
-    def evaluate(self):
-        result = self.expression()
+        
+    def expression(self):
+        result = self.logical_or()
         if self.get_current() != 'EOF':
             raise SyntaxError('Unexpected )')
         return result
+    
+    def logical_or(self):
+        result = self.logical_and()
+        while self.get_current() == 'or':
+            self.eat()
+            right = self.logical_and()
+            result = result or right
+        return result
+    
+    def logical_and(self):
+        result = self.equality()
+        while self.get_current() == 'and':
+            self.eat()
+            right = self.equality()
+            result = result and right
+        return result
+    
+    def equality(self):
+        left = self.comparison()
+        while self.get_current() in ('!=', '=='):
+            op = self.get_current()
+            self.eat()
+            if op == '==':
+                left = (left == self.comparison())
+            else:
+                left = (left != self.comparison())
+        return left
+            
+    
+    def comparison(self):
+        left = self.additive()
+        while self.get_current() in ('>=', '<=', '>', '<'):
+            op = self.get_current()
+            self.eat()
+            if op == '>=':
+                left = left >= self.additive()
+            elif op == '>':
+                left = left > self.additive()
+            elif op == '<=':
+                left = left <= self.additive()
+            else:
+                left = left < self.additive()
+        return left
 
-
-    def expression(self):
-        result = self.term()
+    def additive(self):
+        result = self.multiplicative()
         while self.get_current() in ('+', '-'):
             op = self.get_current()
             self.eat()
             if op == '+':
-                result += self.term()
+                result += self.multiplicative()
             else:
-                result -= self.term()
-
+                result -= self.multiplicative()
         return result
-    
 
-    def term(self):
-        im_result = self.factor()
+    def multiplicative(self):
+        im_result = self.unary()
         while self.get_current() in ('*', '/'):
             op = self.get_current()
             self.eat()
             if op == '*':
-                im_result *= self.factor()
+                im_result *= self.unary()
             elif op == '/':
-                div = self.factor()
+                div = self.unary()
                 if div != 0: im_result /= div
                 else: raise ZeroDivisionError('You cannot divide by zero')
             else: 
                 self.factor()
         return im_result
-        
+    
+    def unary(self):
+        while self.get_current() in ('-', '+'):
+            un = self.get_current()
+            self.eat()
+            if un == '-':
+                result = -(self.unary())
+            else:
+                result = self.unary()
+            return result
+        return self.power()
+
+    def power(self):
+        left = self.factor()
+        if self.get_current() == '^':
+            self.eat()
+            right = self.unary()
+            result = left ** right
+            return result
+        return left
+
     def factor(self):
         num = self.get_current()
         if isinstance(num, int):
@@ -57,7 +115,7 @@ class Parser:
             return num
         elif num == '(':
             self.eat()
-            brackets_result = self.expression()
+            brackets_result = self.logical_or()
             if brackets_result == None:
                 raise SyntaxError('Empty brackets are not supporting right now')
             if self.get_current() != ')':
--- test_main.py
+++ test_main.py
@@ -1,207 +1,305 @@
 import pytest
 
-from main import Converter, Tokeniser, Parser
+from converter import Converter
+from parser import Parser
+from tokens import Tokeniser
 
 
-def evaluate(expr: str):
-    converted = Converter.convert(expr)
+
+
+def evaluate(expression):
+    converted = Converter.convert(expression)
     tokens = Tokeniser.tokenise(converted)
     parser = Parser(tokens)
-    return parser.evaluate()
+    return parser.expression()
 
+class TestUnary:
+    def test_unary_minus_simple(self):
+        assert evaluate('-2') == -2
 
-# ---------- Converter ----------
+    # def test_unary_minus_float(self):
+    #     assert evaluate('-3.0') == -3.0
 
-def test_converter_empty_string():
-    assert Converter.convert('') == ['EOF']
+    def test_unary_plus(self):
+        assert evaluate('+5') == 5
 
+    def test_double_unary_minus(self):
+        assert evaluate('--2') == 2
 
-def test_converter_only_spaces():
-    assert Converter.convert('   ') == ['EOF']
+    def test_triple_unary_minus(self):
+        assert evaluate('---2') == -2
 
+    def test_unary_minus_in_addition(self):
+        assert evaluate('1 + -2') == -1
 
-def test_converter_one_number():
-    assert Converter.convert('7') == [7, 'EOF']
+    def test_unary_minus_in_multiplication(self):
+        assert evaluate('2 * -3') == -6
 
+    def test_unary_minus_with_parentheses(self):
+        assert evaluate('-(1 + 2)') == -3
 
-def test_converter_multi_digit_number():
-    assert Converter.convert('123') == [123, 'EOF']
+    def test_unary_minus_nested_parens(self):
+        assert evaluate('-(-2)') == 2
 
+    def test_unary_minus_complex(self):
+        assert evaluate('-(2 + 3) * 2') == -10
 
-def test_converter_simple_expression():
-    assert Converter.convert('12 + 3') == [12, '+', 3, 'EOF']
+    def test_unary_minus_before_factor_in_term(self):
+        assert evaluate('10 + -2 * 3') == 4
 
+    def test_unary_minus_applied_to_paren_expression(self):
+        assert evaluate('2 * -(3 + 4)') == -14
 
-def test_converter_expression_with_parentheses():
-    assert Converter.convert('(12 + 3)') == ['(', 12, '+', 3, ')', 'EOF']
 
 
-def test_converter_nested_parentheses():
-    assert Converter.convert('((2+3)*4)') == ['(', '(', 2, '+', 3, ')', '*', 4, ')', 'EOF']
+class TestPower:
+    def test_simple_power(self):
+        assert evaluate('2 ^ 3') == 8
 
+    def test_power_of_one(self):
+        assert evaluate('5 ^ 1') == 5
 
-# ---------- Tokeniser ----------
+    def test_power_of_zero(self):
+        assert evaluate('5 ^ 0') == 1
 
-def test_tokeniser_number():
-    tokens = Tokeniser.tokenise([12, 'EOF'])
-    assert tokens[0]['type'] == 'NUM'
-    assert tokens[0]['value'] == 12
-    assert tokens[1]['type'] == 'EOF'
+    def test_zero_to_power(self):
+        assert evaluate('0 ^ 5') == 0
 
+    def test_right_associativity(self):
+        assert evaluate('2 ^ 3 ^ 2') == 512
 
-def test_tokeniser_add():
-    tokens = Tokeniser.tokenise(['+', 'EOF'])
-    assert tokens[0]['type'] == 'ADD'
-    assert tokens[0]['value'] == '+'
-    assert tokens[1]['type'] == 'EOF'
+    def test_power_with_multiplication(self):
+        assert evaluate('2 * 3 ^ 2') == 18
 
+    def test_power_with_addition(self):
+        assert evaluate('1 + 2 ^ 3') == 9
 
-def test_tokeniser_parentheses():
-    tokens = Tokeniser.tokenise(['(', 2, ')', 'EOF'])
-    assert [tok['type'] for tok in tokens] == ['LPAREN', 'NUM', 'RPAREN', 'EOF']
-    assert [tok['value'] for tok in tokens] == ['(', 2, ')', 'EOF']
+    def test_unary_minus_and_power(self):
+        assert evaluate('-2 ^ 3') in (-8, -8)
 
+    def test_power_in_parens(self):
+        assert evaluate('(2 ^ 3) ^ 2') == 64
 
-def test_tokeniser_full_sequence():
-    tokens = Tokeniser.tokenise(['(', 12, '+', 3, ')', '*', 2, 'EOF'])
-    assert [tok['type'] for tok in tokens] == [
-        'LPAREN', 'NUM', 'ADD', 'NUM', 'RPAREN', 'MUL', 'NUM', 'EOF'
-    ]
+    def test_power_and_paren_order(self):
+        assert evaluate('2 ^ (3 + 1)') == 16
 
 
-# ---------- Simple evaluation ----------
 
-def test_eval_number_only():
-    assert evaluate('5') == 5
+class TestComparison:
+    def test_less_than_true(self):
+        assert evaluate('1 < 2') == True
 
+    def test_less_than_false(self):
+        assert evaluate('2 < 1') == False
 
-def test_eval_add():
-    assert evaluate('1 + 2') == 3
+    def test_less_or_equal_equal(self):
+        assert evaluate('2 <= 2') == True
 
+    def test_less_or_equal_less(self):
+        assert evaluate('1 <= 2') == True
 
-def test_eval_sub():
-    assert evaluate('7 - 4') == 3
+    def test_less_or_equal_greater(self):
+        assert evaluate('3 <= 2') == False
 
+    def test_greater_than_true(self):
+        assert evaluate('5 > 3') == True
 
-def test_eval_mul():
-    assert evaluate('2 * 3') == 6
+    def test_greater_than_false(self):
+        assert evaluate('1 > 3') == False
 
+    def test_greater_or_equal_equal(self):
+        assert evaluate('3 >= 3') == True
 
-def test_eval_div():
-    assert evaluate('8 / 2') == 4
+    def test_greater_or_equal_greater(self):
+        assert evaluate('4 >= 3') == True
 
+    def test_greater_or_equal_less(self):
+        assert evaluate('2 >= 3') == False
 
-def test_eval_spaces():
-    assert evaluate('  12   +   3  ') == 15
+    def test_comparison_with_arithmetic_left(self):
+        assert evaluate('1 + 2 < 5') == True
 
+    def test_comparison_with_arithmetic_both(self):
+        assert evaluate('2 * 3 > 1 + 4') == True
 
-def test_eval_multi_digit():
-    assert evaluate('123 + 7') == 130
+    def test_comparison_with_parens(self):
+        assert evaluate('(2 + 3) >= 5') == True
 
+    def test_comparison_with_unary(self):
+        assert evaluate('-1 < 0') == True
 
-# ---------- Priority without parentheses ----------
+    def test_comparison_with_power(self):
+        assert evaluate('2 ^ 3 > 7') == True
 
-def test_eval_priority_mul_before_add():
-    assert evaluate('1 + 2 * 3') == 7
 
 
-def test_eval_priority_mul_before_sub():
-    assert evaluate('10 - 2 * 3') == 4
+class TestEquality:
+    def test_equal_numbers_true(self):
+        assert evaluate('1 == 1') == True
 
+    def test_equal_numbers_false(self):
+        assert evaluate('1 == 2') == False
 
-def test_eval_priority_left_to_right_same_level_mul_div():
-    assert evaluate('20 / 5 * 2') == 8
+    def test_not_equal_true(self):
+        assert evaluate('1 != 2') == True
 
+    def test_not_equal_false(self):
+        assert evaluate('1 != 1') == False
 
-def test_eval_priority_left_to_right_same_level_add_sub():
-    assert evaluate('10 - 3 - 2') == 5
+    def test_equality_with_arithmetic(self):
+        assert evaluate('1 + 2 == 3') == True
 
+    def test_equality_arithmetic_both_sides(self):
+        assert evaluate('2 * 3 == 3 * 2') == True
 
-def test_eval_priority_mixed():
-    assert evaluate('8 / 2 + 3 * 2') == 10
+    def test_equality_with_comparison(self):
+        # (1 < 2) == True
+        assert evaluate('(1 < 2) == 1') == True
 
+    def test_inequality_arithmetic(self):
+        assert evaluate('2 + 2 != 5') == True
 
-# ---------- Parentheses ----------
+    def test_equality_with_power(self):
+        assert evaluate('2 ^ 3 == 8') == True
 
-def test_eval_single_parenthesized_number():
-    assert evaluate('(2)') == 2
+    def test_equality_with_unary(self):
+        assert evaluate('-3 == -3') == True
 
+    def test_equality_negative_and_positive(self):
+        assert evaluate('-1 != 1') == True
 
-def test_eval_simple_parentheses():
-    assert evaluate('(1 + 2)') == 3
+class TestLogicalAnd:
+    def test_true_and_true(self):
+        assert evaluate('1 and 1')
 
+    def test_true_and_false(self):
+        assert not evaluate('1 and 0')
 
-def test_eval_parentheses_change_priority():
-    assert evaluate('(1 + 2) * 3') == 9
+    def test_false_and_true(self):
+        assert not evaluate('0 and 1')
 
+    def test_false_and_false(self):
+        assert not evaluate('0 and 0')
 
-def test_eval_parentheses_right_side():
-    assert evaluate('2 * (3 + 4)') == 14
+    def test_and_with_comparison(self):
+        assert evaluate('1 < 2 and 3 > 1')
 
+    def test_and_with_comparison_false(self):
+        assert not evaluate('1 < 2 and 3 < 1')
 
-def test_eval_nested_parentheses():
-    assert evaluate('((2 + 3) * 4)') == 20
+    def test_and_with_equality(self):
+        assert evaluate('1 == 1 and 2 == 2')
 
+    def test_and_priority_over_or(self):
+        assert evaluate('0 or 1 and 1')
 
-def test_eval_double_nested_parentheses():
-    assert evaluate('2 * ((1 + 2) * (3 + 1))') == 24
+    def test_and_chain(self):
+        assert evaluate('1 and 1 and 1')
 
+    def test_and_chain_with_zero(self):
+        assert not evaluate('1 and 0 and 1')
 
-def test_eval_parentheses_inside_parentheses():
-    assert evaluate('(2 + (3 * 4))') == 14
+    def test_and_with_arithmetic(self):
+        assert evaluate('(2 + 2 == 4) and (3 * 3 == 9)')
 
+    def test_and_with_unary(self):
+        assert evaluate('-1 < 0 and 1 > 0')
 
-def test_eval_complex_expression():
-    assert evaluate('((12 + 3) * 2 - 4) / 2') == 13
+    def test_and_with_nested_parens(self):
+        assert evaluate('(1 + 2 == 3) and (4 - 1 == 3)')
 
 
-# ---------- Error cases ----------
+class TestLogicalOr:
+    def test_true_or_true(self):
+        assert evaluate('1 or 1')
 
-def test_division_by_zero():
-    with pytest.raises(ZeroDivisionError):
-        evaluate('10 / 0')
+    def test_true_or_false(self):
+        assert evaluate('1 or 0')
 
+    def test_false_or_true(self):
+        assert evaluate('0 or 1')
 
-def test_division_by_zero_in_parentheses():
-    with pytest.raises(ZeroDivisionError):
-        evaluate('10 / (5 - 5)')
+    def test_false_or_false(self):
+        assert not evaluate('0 or 0')
 
+    def test_or_with_comparison(self):
+        assert evaluate('5 < 1 or 1 < 5')
 
-def test_missing_closing_parenthesis():
-    with pytest.raises(SyntaxError):
-        evaluate('(1 + 2')
+    def test_or_with_equality(self):
+        assert evaluate('1 == 2 or 2 == 2')
 
+    def test_or_chain(self):
+        assert evaluate('0 or 0 or 1')
 
-def test_extra_closing_parenthesis():
-    with pytest.raises(SyntaxError):
-        evaluate('1 + 2)')
+    def test_or_chain_all_false(self):
+        assert not evaluate('0 or 0 or 0')
 
+    def test_or_with_arithmetic(self):
+        assert evaluate('2 + 2 == 5 or 2 + 2 == 4')
 
-def test_nested_missing_closing_parenthesis():
-    with pytest.raises(SyntaxError):
-        evaluate('((1 + 2) * 3')
+    def test_or_with_and_precedence(self):
+        assert evaluate('1 or 0 and 0')
 
+    def test_or_complex(self):
+        assert evaluate('(1 == 2 or 2 == 2) and (3 > 1 or 5 < 1)')
 
-def test_empty_parentheses():
-    with pytest.raises(SyntaxError):
-        evaluate('()')
 
+class TestComplex:
+    def test_full_arithmetic(self):
+        assert evaluate('(1 + 2 * 3 - 4) / (5 - 4)') == 3
 
-def test_operator_at_end():
-    with pytest.raises(Exception):
-        evaluate('1 +')
+    def test_power_inside_comparison(self):
+        assert evaluate('2 ^ 10 > 1000') == True
 
+    def test_unary_in_comparison(self):
+        assert evaluate('-5 + 3 < 0') == True
 
-def test_operator_at_start():
-    with pytest.raises(Exception):
-        evaluate('* 2')
+    def test_nested_parens_arithmetic(self):
+        assert evaluate('((2 + 3) * (4 - 1)) / 5') == 3
 
+    def test_comparison_and_equality(self):
+        assert evaluate('(1 < 2) == 1') == True
 
-def test_two_operators_in_row():
-    with pytest.raises(Exception):
-        evaluate('1 ++ 2')
+    def test_double_comparison_in_and(self):
+        assert evaluate('1 < 2 and 2 < 3 and 3 < 4') == True
 
+    def test_or_of_comparisons(self):
+        assert evaluate('5 < 1 or 1 < 5 or 3 == 3') == True
 
-def test_invalid_character():
-    with pytest.raises(Exception):
-        evaluate('2 + a')
+    def test_deep_nesting_logical(self):
+        assert evaluate('(1 + 1 == 2) and (2 * 2 == 4) or (5 == 6)') == True
+
+    def test_unary_and_comparison(self):
+        assert evaluate('(-1 + 3) > 1 and (2 ^ 2) == 4') == True
+
+    def test_power_with_comparison_and_logical(self):
+        assert evaluate('2 ^ 3 == 8 and 3 ^ 2 == 9') == True
+
+    def test_complex_or_and_chain(self):
+        assert evaluate('(0 == 1 or 1 == 1) and (2 + 2 == 4 or 3 + 3 == 7)') == True
+
+    def test_arithmetic_both_sides_of_equality_in_and(self):
+        assert evaluate('2 * 5 == 10 and 12 / 4 == 3') == True
+
+    def test_unary_double_minus_in_expression(self):
+        assert evaluate('--2 + --3 == 5') == True
+
+    def test_unary_applied_to_expression_in_comparison(self):
+        assert evaluate('-(2 + 3) == -5') == True
+
+    def test_large_complex_expression(self):
+        assert evaluate('((1 + 2) * (3 ^ 2) - 4) > 20 and (0 or 1)') == True
+
+
+class TestErrors:
+    def test_division_by_zero(self):
+        with pytest.raises(ZeroDivisionError):
+            evaluate('10 / 0')
+
+    def test_division_by_zero_expression(self):
+        with pytest.raises(ZeroDivisionError):
+            evaluate('10 / (5 - 5)')
+
+    def test_missing_closing_paren(self):
+        with pytest.raises(SyntaxError):
+            evaluate('(1-1')
